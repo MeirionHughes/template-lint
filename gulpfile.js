6f4b8d5fc3b0be93ac763ef4c4bcf6ccdac79d1f
Meirion Hughes <mhughes@geologging.com>
2016-05-29 21:06:32 +0100

    clean build. fix typings in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -10,66 +10,70 @@ var plumber = require('gulp-plumber');
 var sourcemap = require('gulp-sourcemaps');
 
 var paths = {
-    source : "source/",
-    output : "dist/",
-    spec : "spec/"
+    source: "source/",
+    output: "dist/",
+    spec: "spec/"
 }
 
 gulp.task('compile', function () {
-    var project = ts.createProject('tsconfig.json');
 
-    var tsResult = gulp        
-        .src([
-            '!' + paths.source + '**/*spec.ts',
-            paths.source + '**/*.ts',
-            'typings/index.d.ts'
-        ])
+    var source = ['!' + paths.source + '**/*spec.ts',
+        paths.source + '**/*.ts',
+        'typings/index.d.ts'];
+
+    var tsResult = gulp
+        .src(source)
         .pipe(sourcemap.init())
-        .pipe(ts(project));        
-        
+        .pipe(ts({
+            target: "es6",
+            module: "commonjs",
+            declaration: true
+        }));
+
     return merge([
-		tsResult.dts.pipe(gulp.dest(paths.output)),
-		tsResult.js
-            .pipe(sourcemap.write('.', {sourceRoot: '../source'}))   
+        tsResult.dts.pipe(gulp.dest(paths.output)),
+        tsResult.js.pipe(sourcemap.write('.', { sourceRoot: '../source' }))
             .pipe(gulp.dest(paths.output))
-	]);
+    ]);
 });
 
 gulp.task('compile-tests', ['compile'], function () {
-    var project = ts.createProject('tsconfig.json');
+    var source = [paths.source + '**/*spec.ts',
+                 'typings/index.d.ts'];
 
-    var tsResult = gulp.src([
-            paths.source + '**/*spec.ts',
-            'typings/index.d.ts'
-        ])
+    var tsResult = gulp.src(source)
         .pipe(sourcemap.init())
-        .pipe(ts(project));        
-        
+        .pipe(ts({
+            target: "es6",
+            module: "commonjs",
+            declaration: false
+        }));
+
     return tsResult.js
-        .pipe(sourcemap.write('.',  {sourceRoot: '../source'}))
+        .pipe(sourcemap.write('.', { sourceRoot: '../source' }))
         .pipe(gulp.dest(paths.spec));
 });
 
-gulp.task('test', ['compile-tests'], function() {
-   return gulp.src('spec/*.js')
-      .pipe(plumber())
-      .pipe(jasmine({verbose:true}));
+gulp.task('test', ['compile-tests'], function () {
+    return gulp.src('spec/*.js')
+        .pipe(plumber())
+        .pipe(jasmine({ verbose: true }));
 });
 
 gulp.task('watch', ['test'], function () {
-    
+
     gulp.watch(paths.source + '**/*.ts', ['test']);
-        
+
 });
 
 gulp.task('flush', function () {
-  for (var prop in require.cache) {
-    if (prop.indexOf("node_modules") === -1) {
-      delete require.cache[prop];
+    for (var prop in require.cache) {
+        if (prop.indexOf("node_modules") === -1) {
+            delete require.cache[prop];
+        }
     }
-  }
 });
 
-gulp.task('default', function() {
-   return runSequence('compile', 'compile-tests', 'test');
+gulp.task('default', function () {
+    return runSequence('compile', 'compile-tests', 'test');
 });
