16510e64d6e611dabe50e64d50fc52e4987caae0
Meirion Hughes <mhughes@geologging.com>
2016-06-21 00:03:31 +0100

    initialise data to empty object

--- source/parser-node.ts
+++ source/parser-node.ts
@@ -7,6 +7,6 @@ import {Attribute} from 'parse5';
 export class ParserNode {
     public data:any;
     constructor(public scope: string, public name: string, public attrs:Attribute[], public location: StartTagLocationInfo) {
-        this.data = null;
+        this.data = {};
     }
 }
