d7316a65645211a36b80a754ac9467c11dde68f6
Meirion Hughes <mhughes@geologging.com>
2016-06-20 19:45:29 +0100

    fix typings

--- source/parser-node.ts
+++ source/parser-node.ts
@@ -5,7 +5,7 @@ import {Attribute} from 'parse5';
  *  Node in traversal stack
  */
 export class ParserNode {
-    public data:{};    
+    public data:any;
     constructor(public scope: string, public name: string, public attrs:Attribute[], public location: StartTagLocationInfo) {
         this.data = null;
     }
